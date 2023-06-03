1182a0ac9972b696e0f8a4533a293e7e62e8ef46
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-04 01:09:14 +0530

    about

--- nextpractice/src/pages/contact.js
+++ nextpractice/src/pages/contact.js
@@ -1,17 +1,18 @@
 import style from "../styles/home.module.css";
-import Link from "next/link"; 
+import Link from "next/link";  
 
-const about = () => {
+
+const contact = () => {
   return (
     <div className={style.headbox}>
       <div className={style.nav}>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link> 
+        <Link href="/contact">Contact</Link>
       </div>
-      <h1 className={style.box1}>This is About Page</h1>
+      <h1 className={style.box1}>This is Contact Section. </h1>
     </div>
-  );
-};
+  )
+}
 
-export default about;
+export default contact
--- nextpractice/src/pages/index.js
+++ nextpractice/src/pages/index.js
@@ -1,17 +1,16 @@
-import style from "../styles/home.module.css";
+import styles from "../styles/home.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main
-      className={style.headbox}
-    >
-      <div className={style.nav}>
+    <main className={styles.headbox}>
+      <div className={styles.nav}>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
       </div>
       <div>
-        <h1 className={style.box1}>This is Nextjs Home Page</h1>
+        <h1 className={styles.box1}>This is Nextjs Home Page</h1>
       </div>
     </main>
   );
