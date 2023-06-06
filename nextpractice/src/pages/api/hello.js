bfccadbcd0035db4df2fc3164ca291a36e50f3a7
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-06 21:37:22 +0530

    dbjson data

--- nextpractice/src/pages/api/hello.js
+++ nextpractice/src/pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from "node:fs";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  fs.readFile("blogdata/db.json", "utf-8", (err, data) => {
+    // let new_data = JSON.parse(data);
+    return res.status(200).json(JSON.parse(data));
+  });
 }
