5963516939c74602e8cc023ba4d5488308299ea4
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-13 02:15:55 +0530

    data

--- ecom/src/compoents/home/Home.jsx
+++ ecom/src/compoents/home/Home.jsx
@@ -5,19 +5,31 @@ const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <div className={style.home_top}> 
-        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img1.jpg" alt="" />
-        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img2.jpg" alt="" /> 
+      <div className={style.home_top}>
+        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img4.jpg" alt="" />
+        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img6.jpg" alt="" />
         <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img3.jpg" alt="" />
-        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img7.jpg" alt="" />
-      </div> 
+        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img9.jpg" alt="" />
+      </div>
       <div>
-
+        <h2>Shop New Arrivals</h2>
+        <button>Shop Women</button>
+        <button>Shop Men</button>
+        <button>Shop Girls</button>
+        <button>Shop Boys</button>
+      </div>
+      <div className={style.vid_div}>
+        <h1>More Stories</h1>
+        <video autoPlay muted loop>
+          <source src="https://www.jcrew.com/brand_creative/homepage2023/06-Jun/SITE_EntryAnimation_1256x493_v2_hb.mp4" type="video/mp4" /> 
+        </video> 
+        <h3>Step into our virtual beach house…</h3> 
+        <h4>A new way to shop and explore the world of J.Crew.</h4> 
+        <a href="#">Discover, play & find something new</a> 
       </div>
     </div>
   );
 };
 
-export default Home; 
-
+export default Home;  
 
