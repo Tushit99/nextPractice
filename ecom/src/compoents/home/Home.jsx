cf438e77e051eee39b10c3b58dba70b3d4ffdc87
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-12 22:53:36 +0530

    data

--- ecom/src/compoents/home/Home.jsx
+++ ecom/src/compoents/home/Home.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';  
-import style from "./Home.module.css"; 
-import axios from 'axios';
+import React, { useEffect } from "react";
+import style from "./Home.module.css";
 
-const Home = () => { 
-
-  
+const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>  
-      
+      <h1>Home Page</h1>
+      <div>
+        <img src="https://www.jcrew.com/brand_creative/homepage2023/06-Jun/2023jun_0606_hp_w_img0b.jpg" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" /> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home 
+export default Home;
