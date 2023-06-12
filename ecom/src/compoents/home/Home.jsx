4581dd576adaf13549b17d5ff221cf3a39f806de
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-12 23:21:14 +0530

    data

--- ecom/src/compoents/home/Home.jsx
+++ ecom/src/compoents/home/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <div>
-        <img src="https://www.jcrew.com/brand_creative/homepage2023/06-Jun/2023jun_0606_hp_w_img0b.jpg" alt="" />
+      <div className={style.home_top}>
+        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img1.jpg" alt="" />
+        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img2.jpg" alt="" /> 
         <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img3.jpg" alt="" />
         <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img4.jpg" alt="" />
-        <img src="https://www.jcrew.com/brand_creative/2023/202306-Jun/linen/2023jun_0523_linen_w_img7.jpg" alt="" /> 
       </div>
     </div>
   );
