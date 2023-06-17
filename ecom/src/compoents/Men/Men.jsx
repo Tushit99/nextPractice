ad0a5743771a4442a4eb95d15f57cb9ae4688a01
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-18 02:01:30 +0530

    sort

--- ecom/src/compoents/Men/Men.jsx
+++ ecom/src/compoents/Men/Men.jsx
@@ -1,47 +1,68 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useLocation, useSearchParams } from 'react-router-dom';
-import stl from "./Men.module.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
+import stl from "./Men.module.css"; 
 
 const Men = () => {
     const [searchParams, setSearchParams] = useSearchParams();
-    const [data, setData] = useState([])
-    const [page, setPage] = useState(1);
-    const [rating, setRating] = useState("");
-    const [brand, setBrand] = useState([]);
+    const [data, setData] = useState([]);
+    const [page, setPage] = useState(1); 
+    const initialRating = searchParams.get("rating"); 
+    const initalBrand = searchParams.getAll("brand");  
+    const [rating, setRating] = useState(initialRating || null);  
     const location = useLocation();
+    const limit = 12;
+    const [brand, setBrand] = useState(initalBrand || []);
+
+    // const handleChange = (e) => {
+    //     let newCat = [...brand];
+    //     let value = e.target.value; 
+
+    //     if (newCat.includes(value)) {
+    //         newCat.splice(newCat.indexOf(value), 1);
+    //     } else {
+    //         newCat.push(value);
+    //     }
+    //     setBrand(newCat);
+    // };  
+
+    const handelchang =(a)=>{
+        setRating(a); 
+    }
 
     useEffect(() => {
-        let isPage = searchParams.get("page") || 1;
-        let israting = searchParams.get("rating") || "";
-        let isbrand = searchParams.getAll("brand") || [];
 
-        setPage(isPage);
-        setRating(israting);
-        setBrand(isbrand);
+        let obj = {
+            page: page
+        }
+        rating && (obj.rating = rating); 
+        
+        axios
+            .get(`https://grandscale.onrender.com/mensShirt/${location.search}`)
+            .then((e) => {
+                setData(e.data);
+                console.log(e.data);
+            });
 
-        axios.get(`https://grandscale.onrender.com/mensShirt/${location.search}`).then((e) => {
-            setData(e.data);
-            console.log(e.data);
-        })
+        setSearchParams(obj);
 
-    }, [location.search])
+    }, [location.search, rating]);
 
     return (
         <div>
             <h1>Men's Page</h1>
             <div className={stl.Product_head}>
                 <div className={stl.ratings}>
-                    <div className={stl.rate}>
-                        <input type="radio" name="radio" onClick={() => setRating(3)} />
+                    <div className={stl.rate} >
+                        <input type="radio" name="radio" onClick={() => handelchang(3)} defaultChecked={rating==3} /> 
                         {rating == 3 ? (
                             <h4>&#9733; &#9733; &#9733; &#9734; &#9734; </h4>
                         ) : (
                             <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
-                        )}
+                        )} 
                     </div>
                     <div className={stl.rate}>
-                        <input type="radio" name="radio" onClick={() => setRating(4)} />
+                        <input type="radio" name="radio" onClick={() => handelchang(4)} defaultChecked={rating==4} />
                         {rating == 4 ? (
                             <h4>&#9733; &#9733; &#9733; &#9733; &#9734; </h4>
                         ) : (
@@ -49,14 +70,14 @@ const Men = () => {
                         )}
                     </div>
                     <div className={stl.rate}>
-                        <input type="radio" name="radio" onClick={() => setRating(5)} />
+                        <input type="radio" name="radio" onClick={() => handelchang(5)} defaultChecked={rating==5} />
                         {rating == 5 ? (
                             <h4>&#9733; &#9733; &#9733; &#9733; &#9733; </h4>
                         ) : (
                             <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
                         )}
                     </div>
-                </div>
+                </div> 
                 <div className={stl.data_list}>
                     {data?.map((e, i) => (
                         <div key={i + 1} className={stl.data_detail}>
@@ -64,12 +85,13 @@ const Men = () => {
                             <h3>{e.tags} </h3>
                             <p>brand: {e.brand}</p>
                             <p>Price: ₹{e.price}</p>
+                            <p>rating: {e.rating}</p>
                         </div>
                     ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Men
+export default Men;
--- ecom/src/compoents/product/Product.jsx
+++ ecom/src/compoents/product/Product.jsx
@@ -2,70 +2,68 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useSearchParams } from "react-router-dom";
 import stl from "./Product.module.css";
+import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
 
 const Product = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const [data, setData] = useState([]);
-    const [page, setPage] = useState(1);
-    const [rating, setRating] = useState(3);
-    const [brand, setBrand] = useState([]);
+    const [page, setPage] = useState(1); 
+    const initialRating = searchParams.get("rating"); 
+    const initalBrand = searchParams.getAll("brand");  
+    const [rating, setRating] = useState(initialRating || null);  
     const location = useLocation();
     const limit = 12;
-    const initalState = searchParams.getAll("brand"); 
-    const [category, setCategory] = useState(initalState || []);
+    const [brand, setBrand] = useState(initalBrand || []);
+
+    // const handleChange = (e) => {
+    //     let newCat = [...brand];
+    //     let value = e.target.value; 
+
+    //     if (newCat.includes(value)) {
+    //         newCat.splice(newCat.indexOf(value), 1);
+    //     } else {
+    //         newCat.push(value);
+    //     }
+    //     setBrand(newCat);
+    // };  
+
+    const handelchang =(a)=>{
+        setRating(a); 
+    }
 
     useEffect(() => {
-        let isPage = searchParams.get("page") || 1;
-        let israting = searchParams.get("rating") || "";
-        let isbrand = searchParams.getAll("brand") || [];
 
-        setPage(isPage);
-        setRating(israting);
-        setBrand(isbrand);
-
-        const handleChange = (e) => {
-            let newCat = [...category];
-            let value = e.target.value;
-
-            if (newCat.includes(value)) {
-                newCat.splice(newCat.indexOf(value), 1);
-            } else {
-                newCat.push(value);
-            }
-            setCategory(newCat);
-        };
-
+        let obj = {
+            page: page
+        }
+        rating && (obj.rating = rating); 
+        
         axios
-            .get(`https://grandscale.onrender.com/womensTop/${location.search}`)
+            .get(`https://grandscale.onrender.com/mensShirt/${location.search}`)
             .then((e) => {
                 setData(e.data);
                 console.log(e.data);
             });
 
-        let obj = {
-            page: isPage
-        }
-        israting && (obj.rating = israting);
-
         setSearchParams(obj);
 
-    }, [location.search]);
+    }, [location.search, rating]); 
 
     return (
         <div>
             <h1>Women's Page</h1>
             <div className={stl.Product_head}>
                 <div className={stl.ratings}>
-                    <div className={stl.rate}>
-                        <input type="radio" name="radio" onClick={() => setRating(3)} />
+                    <div className={stl.rate} >
+                        <input type="radio" name="radio" onClick={() => handelchang(3)} defaultChecked={rating==3} /> 
                         {rating == 3 ? (
                             <h4>&#9733; &#9733; &#9733; &#9734; &#9734; </h4>
                         ) : (
                             <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
-                        )}
+                        )} 
                     </div>
                     <div className={stl.rate}>
-                        <input type="radio" name="radio" onClick={() => setRating(4)} />
+                        <input type="radio" name="radio" onClick={() => handelchang(4)} defaultChecked={rating==4} />
                         {rating == 4 ? (
                             <h4>&#9733; &#9733; &#9733; &#9733; &#9734; </h4>
                         ) : (
@@ -73,14 +71,14 @@ const Product = () => {
                         )}
                     </div>
                     <div className={stl.rate}>
-                        <input type="radio" name="radio" onClick={() => setRating(5)} />
+                        <input type="radio" name="radio" onClick={() => handelchang(5)} defaultChecked={rating==5} />
                         {rating == 5 ? (
                             <h4>&#9733; &#9733; &#9733; &#9733; &#9733; </h4>
                         ) : (
                             <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
                         )}
                     </div>
-                </div>
+                </div> 
                 <div className={stl.data_list}>
                     {data?.map((e, i) => (
                         <div key={i + 1} className={stl.data_detail}>
@@ -88,6 +86,7 @@ const Product = () => {
                             <h3>{e.tags} </h3>
                             <p>brand: {e.brand}</p>
                             <p>Price: ₹{e.price}</p>
+                            <p>rating: {e.rating}</p>
                         </div>
                     ))}
                 </div>
