3bf54ef4c3b214230fa213ecdc432c93449744b3
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-17 19:25:50 +0530

    mens-page

--- ecom/src/compoents/PublicRoute.jsx
+++ ecom/src/compoents/PublicRoute.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './home/Home'
 import Product from './product/Product'
+import Men from './Men/Men'
 
 const PublicRoute = () => {
   return (
     <Routes>
         <Route path="/" element={<Home />} />  
-        <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<Product />} /> 
+        <Route path='/men' element={<Men />} />  
     </Routes> 
   )
 }
