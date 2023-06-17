1ba0c15214a4d00a36f3273c00e2b475a86954a7
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-17 23:23:32 +0530

    mens-page-sort

--- ecom/src/compoents/product/Product.jsx
+++ ecom/src/compoents/product/Product.jsx
@@ -4,80 +4,96 @@ import { useLocation, useSearchParams } from "react-router-dom";
 import stl from "./Product.module.css";
 
 const Product = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
-  const [rating, setRating] = useState(3);
-  const [brand, setBrand] = useState([]);
-  const location = useLocation();
-  const limit = 12;
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
+    const [rating, setRating] = useState(3);
+    const [brand, setBrand] = useState([]);
+    const location = useLocation();
+    const limit = 12;
+    const initalState = searchParams.getAll("brand"); 
+    const [category, setCategory] = useState(initalState || []);
 
-  useEffect(() => {
-    let isPage = searchParams.get("page") || 1;
-    let israting = searchParams.get("rating") || "";
-    let isbrand = searchParams.getAll("brand") || [];
+    useEffect(() => {
+        let isPage = searchParams.get("page") || 1;
+        let israting = searchParams.get("rating") || "";
+        let isbrand = searchParams.getAll("brand") || [];
 
-    setPage(isPage);
-    setRating(israting);
-    setBrand(isbrand);
+        setPage(isPage);
+        setRating(israting);
+        setBrand(isbrand);
 
-    axios
-      .get(`https://grandscale.onrender.com/womensTop/${location.search}`)
-      .then((e) => {
-        setData(e.data);
-        console.log(e.data);
-      }); 
+        const handleChange = (e) => {
+            let newCat = [...category];
+            let value = e.target.value;
 
-    let obj={
-        page
-    }
+            if (newCat.includes(value)) {
+                newCat.splice(newCat.indexOf(value), 1);
+            } else {
+                newCat.push(value);
+            }
+            setCategory(newCat);
+        };
 
+        axios
+            .get(`https://grandscale.onrender.com/womensTop/${location.search}`)
+            .then((e) => {
+                setData(e.data);
+                console.log(e.data);
+            });
 
-  }, [location.search]);
+        let obj = {
+            page: isPage
+        }
+        israting && (obj.rating = israting);
 
-  return (
-    <div>
-      <h1>Women's Page</h1>
-      <div className={stl.Product_head}>
-        <div className={stl.ratings}>
-          <div className={stl.rate}>
-            <input type="radio" name="radio" onClick={() => setRating(3)} />
-            {rating == 3 ? (
-              <h4>&#9733; &#9733; &#9733; &#9734; &#9734; </h4>
-            ) : (
-              <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
-            )}
-          </div>
-          <div className={stl.rate}>
-            <input type="radio" name="radio" onClick={() => setRating(4)} />
-            {rating == 4 ? (
-              <h4>&#9733; &#9733; &#9733; &#9733; &#9734; </h4>
-            ) : (
-              <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
-            )}
-          </div>
-          <div className={stl.rate}>
-            <input type="radio" name="radio" onClick={() => setRating(5)} />
-            {rating == 5 ? (
-              <h4>&#9733; &#9733; &#9733; &#9733; &#9733; </h4>
-            ) : (
-              <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
-            )}
-          </div>
-        </div>
-        <div className={stl.data_list}>
-          {data?.map((e, i) => (
-            <div key={i + 1} className={stl.data_detail}>
-              <img src={e.image} alt="product-img" />
-              <h3>{e.tags} </h3>
-              <p>brand: {e.brand}</p>
-              <p>Price: ₹{e.price}</p>
+        setSearchParams(obj);
+
+    }, [location.search]);
+
+    return (
+        <div>
+            <h1>Women's Page</h1>
+            <div className={stl.Product_head}>
+                <div className={stl.ratings}>
+                    <div className={stl.rate}>
+                        <input type="radio" name="radio" onClick={() => setRating(3)} />
+                        {rating == 3 ? (
+                            <h4>&#9733; &#9733; &#9733; &#9734; &#9734; </h4>
+                        ) : (
+                            <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
+                        )}
+                    </div>
+                    <div className={stl.rate}>
+                        <input type="radio" name="radio" onClick={() => setRating(4)} />
+                        {rating == 4 ? (
+                            <h4>&#9733; &#9733; &#9733; &#9733; &#9734; </h4>
+                        ) : (
+                            <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
+                        )}
+                    </div>
+                    <div className={stl.rate}>
+                        <input type="radio" name="radio" onClick={() => setRating(5)} />
+                        {rating == 5 ? (
+                            <h4>&#9733; &#9733; &#9733; &#9733; &#9733; </h4>
+                        ) : (
+                            <h4>&#9734; &#9734; &#9734; &#9734; &#9734; </h4>
+                        )}
+                    </div>
+                </div>
+                <div className={stl.data_list}>
+                    {data?.map((e, i) => (
+                        <div key={i + 1} className={stl.data_detail}>
+                            <img src={e.image} alt="product-img" />
+                            <h3>{e.tags} </h3>
+                            <p>brand: {e.brand}</p>
+                            <p>Price: ₹{e.price}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Product;
