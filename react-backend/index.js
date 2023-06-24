aa7cfba4c8a5b18b25e0cffafb4540379679094f
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-25 03:15:39 +0530

    data

---
+++ react-backend/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./db");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome To HomePage");
+});
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("Connected to DataBase");
+  } catch (error) {
+    console.log("Couldn't connect to DataBase");
+  }
+  console.log(`Server running on port ${process.env.port}`);
+});
