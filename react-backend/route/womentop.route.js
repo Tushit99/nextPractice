83e54cc9be30a8e9485195b4ab52ab54d6a315a0
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-26 02:34:54 +0530

    data

--- react-backend/route/womentop.route.js
+++ react-backend/route/womentop.route.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const { WomensTopModel } = require("../module/womentop.model");
-  
+
 const womentopRouter = express.Router();
 
-womentopRouter.get("/",async(req,res)=>{
-    try { 
-        let data = await WomensTopModel.find(); 
-        res.send({message: data});  
-    } catch (err) {
-        res.status(500).send({message: "Error"}); 
-    }
-})
+womentopRouter.get("/", async (req, res) => {
+  try {
+    let data = await WomensTopModel.find();
+    res.send({ message: data });
+  } catch (err) {
+    res.status(500).send({ message: "Error" });
+  }
+});
 
 womentopRouter.post("/add", async (req, res) => {
-    const payload = req.body;
-    try {
-      const WomensTop = new WomensTopModel(payload);
-      await WomensTop.save(); 
-      res.status(200).send({ message: "item has been Added" });
-    } catch (error) {
-      res.status(400).send({ message: error.message });
-    }
-  }); 
+  const payload = req.body;
+  try {
+    const WomensTop = new WomensTopModel(payload);
+    await WomensTop.save();
+    res.status(200).send({ message: "item has been Added" });
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
 
-module.exports={womentopRouter}
+module.exports = { womentopRouter };
