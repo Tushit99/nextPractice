e59d366989c6cbc35495fed5612ad1b7f0e35e80
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-09-26 17:21:43 +0530

    data

--- converter/converter/src/App.js
+++ converter/converter/src/App.js
@@ -1,13 +1,11 @@
 import "./App.css"; 
 import video from "./compoents/vid.mp4"; 
-import TextTransformer from "./compoents/TextTransformer";
-// import WordTramsform from "./compoents/WordTramsform";
+import TextTransformer from "./compoents/TextTransformer"; 
 
 function App() {
   return (
     <div className="App">  
-      <TextTransformer /> 
-      {/* <WordTramsform />  */}
+      <TextTransformer />  
     </div>
   );
 }
